2b7479b9f1622220cd10ceb75b43a9965b4b02e6
harsh-medicheck <harsh@medicheckhospitals.in>
2025-07-19 23:55:33 +0530

    updated delete

---
+++ src/app/api/admin/data/collab/delete/route.js
@@ -0,0 +1,61 @@
+import { NextResponse } from "next/server";
+import Collaboration from "@/Mongo/Model/DataModels/Collaboration";
+import mongoose from "mongoose";
+
+
+export default async function POST(req){
+    try {
+        await util.connectDB();
+
+        const token = req.cookies.get("dsciAuthToken")?.value || req.headers.get("Authorization")?.split(" ")[1];
+        const decodedToken = decodeTokenPayload(token);
+        if (!decodedToken?.id) {
+            return NextResponse.json(apiResponse({
+                message: "Unauthorized: Invalid or missing token",
+                statusCode: STATUS_CODES.UNAUTHORIZED,
+            }), { status: STATUS_CODES.UNAUTHORIZED });
+        }
+        
+        const { searchParams } = new URL(req.url);
+        const collabId = sanitizeInput(searchParams.get("collaborationId"));
+        const collab = await Collaboration.findById(collabId);
+        if (!collab) {
+            return NextResponse.json(
+                apiResponse({
+                    message: "Collaboration not found",
+                    statusCode: STATUS_CODES.NOT_FOUND,
+                }),
+                { status: STATUS_CODES.NOT_FOUND }
+            );
+        }
+
+        if(collab.isDeleted){
+            return NextResponse.json(
+                apiResponse({
+                    message: "Collaboration already deleted",
+                    statusCode: STATUS_CODES.BAD_REQUEST,
+                }),
+                { status: STATUS_CODES.BAD_REQUEST }
+            );
+        }
+
+        collab.deletedAt = new Date();
+
+
+        
+        collab.isDeleted = true;
+        await collab.save();
+        return NextResponse.json(
+            apiResponse({
+                message: "Collaboration deleted successfully",
+                statusCode: STATUS_CODES.OK,
+            }),
+            { status: STATUS_CODES.OK }
+        );
+
+    } catch (error) {
+        return NextResponse.json(handleError(error), {
+            status: STATUS_CODES.INTERNAL_ERROR,
+        });
+    }
+}
