74e8e0810af02d8cfb6396bdea24e32aa8f48119
Harsh Vatsal <153799575+harshvatsal41@users.noreply.github.com>
2025-08-06 17:18:17 +0530

    Delete src directory

--- src/Mongo/Model/DataModels/FocusArea.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const focusAreaSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
-});
-
-export default mongoose.models.FocusArea || mongoose.model("FocusArea", focusAreaSchema);
